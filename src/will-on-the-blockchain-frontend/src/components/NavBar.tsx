ee8e1a4ca4ad016cd423e9d5634fdaf3f20e1491
Mário Viana <mj.viana1993@hotmail.com>
2023-12-11 22:28:00 +0000

    Feature: Add chakra-ui links to NavBar component

--- src/will-on-the-blockchain-frontend/src/components/NavBar.tsx
+++ src/will-on-the-blockchain-frontend/src/components/NavBar.tsx
@@ -1,22 +1,28 @@
-import {Flex, HStack, Spacer} from "@chakra-ui/react";
+import {Flex, HStack, Spacer, Link} from "@chakra-ui/react";
 import {BiBookHeart} from "react-icons/bi";
 import ColorModeSwitch from "./ColorModeSwitch";
 import {ConnectButton} from "@rainbow-me/rainbowkit";
-import {Link} from "react-router-dom";
+import {Link as ReactRouterLink} from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <Flex paddingLeft={5} paddingRight={5} display="flex" alignItems="center">
-        <Link to={"/"}>
+        <Link as={ReactRouterLink} to={"/"}>
           <BiBookHeart fontSize="60px" />
         </Link>
         <Spacer />
-        <Link to={"/public-wills"}>Public Wills</Link>
+        <Link as={ReactRouterLink} to={"/public-wills"}>
+          Public Wills
+        </Link>
         <Spacer />
-        <Link to={"/create-will"}>Create a Will</Link>
+        <Link as={ReactRouterLink} to={"/create-will"}>
+          Create a Will
+        </Link>
         <Spacer />
-        <Link to={"/how-to-create-will"}>How to</Link>
+        <Link as={ReactRouterLink} to={"/how-to-create-will"}>
+          How to
+        </Link>
         <Spacer />
         <HStack justifyContent={"flex-end"}>
           <ColorModeSwitch />
