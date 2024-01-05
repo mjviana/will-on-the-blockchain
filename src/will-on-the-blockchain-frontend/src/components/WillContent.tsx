34c2ec4c77a82eef6e79c244e09f4bfbc6471149
Mário Viana <mj.viana1993@hotmail.com>
2024-01-05 20:15:43 +0000

    Feature: Add blur effect to private wills

--- src/will-on-the-blockchain-frontend/src/components/WillContent.tsx
+++ src/will-on-the-blockchain-frontend/src/components/WillContent.tsx
@@ -6,12 +6,21 @@ interface WillBodyProps {
 }
 
 const WillContent = ({will}: WillBodyProps) => {
+  const isPrivateWill = !will.isPublic;
   return (
-    <Card mb={5} mx={10} p={10}>
-      <Text as="b" pb={10} fontSize="2xl">
+    <Card
+      mb={5}
+      mx={10}
+      p={10}
+      style={{
+        filter: isPrivateWill ? "blur(8px)" : "none",
+        backdropFilter: isPrivateWill ? "blur(8px)" : "none",
+      }}
+    >
+      <Text as="b" pb={10} fontSize="2xl" style={{userSelect: "none"}}>
         Will Content
       </Text>
-      {will.will}
+      <Text style={{userSelect: "none"}}>{will.will}</Text>
     </Card>
   );
 };
