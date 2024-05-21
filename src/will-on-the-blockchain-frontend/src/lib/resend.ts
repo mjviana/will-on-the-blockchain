4a049a59819feedeccdea2dcadc23ff379aeebd0
Mário Viana <mj.viana1993@hotmail.com>
2024-05-21 20:57:58 +0100

    Fix bug in login functionality

--- src/will-on-the-blockchain-frontend/src/lib/resend.ts
+++ src/will-on-the-blockchain-frontend/src/lib/resend.ts
@@ -1,4 +1,4 @@
 import {Resend} from "resend";
-const RESEND_API_KEY = import.meta.env.VITE_RESEND_API_KEY || "";
+const RESEND_API_KEY = process.env.NEXT_PUBLIC_RESEND_API_KEY || "";
 
 export const resend = new Resend(RESEND_API_KEY);
