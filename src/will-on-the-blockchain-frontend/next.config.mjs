4f1fd2e50378df3cc1405b19c50446bf164ad71d
Mário Viana <mj.viana1993@hotmail.com>
2024-05-23 21:13:26 +0100

    Remove output option and trailingSlash configuration in next.config.js

--- src/will-on-the-blockchain-frontend/next.config.mjs
+++ src/will-on-the-blockchain-frontend/next.config.mjs
@@ -1,29 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Outputs a Single-Page Application (SPA).
+  // output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
   reactStrictMode: true, // Enables React Strict Mode.
-  trailingSlash: true,
   webpack: (config) => {
     config.resolve.fallback = {fs: false, net: false, tls: false};
     config.externals.push("pino-pretty", "encoding");
     return config;
-  },
-  async headers() {
-    return [
-      {
-        // This configuration applies to all routes under the `/api` directory
-        source: "/api/:path*",
-        headers: [
-          {key: "Access-Control-Allow-Origin", value: "*"},
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,POST,PUT,DELETE,OPTIONS",
-          },
-          {key: "Access-Control-Allow-Headers", value: "Content-Type"},
-        ],
-      },
-    ];
   },
 };
 
