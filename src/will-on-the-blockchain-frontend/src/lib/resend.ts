25803b796fdcde4059cdff90da577a9add7ba2e6
Mário Viana <mj.viana1993@hotmail.com>
2024-05-23 22:12:39 +0100

    update the resend.ts file

--- src/will-on-the-blockchain-frontend/src/lib/resend.ts
+++ src/will-on-the-blockchain-frontend/src/lib/resend.ts
@@ -1,4 +1,6 @@
 import {Resend} from "resend";
 const RESEND_API_KEY = process.env.NEXT_PUBLIC_RESEND_API_KEY || "";
 
+console.log("RESEND_API_KEY", RESEND_API_KEY);
+
 export const resend = new Resend(RESEND_API_KEY);
