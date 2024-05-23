4526edf98b6c100a655d60442685df720e5507bc
Mário Viana <mj.viana1993@hotmail.com>
2024-05-24 00:56:55 +0100

    Remove output configuration in next.config.mjs

--- src/will-on-the-blockchain-frontend/next.config.mjs
+++ src/will-on-the-blockchain-frontend/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "undefined", // Outputs a Single-Page Application (SPA).
+  // output: "undefined", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
   // reactStrictMode: true, // Enables React Strict Mode.
   webpack: (config) => {
