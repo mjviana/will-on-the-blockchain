5850f82fcf0b466bc0e22757d925ce0a7ff5e1ea
Mário Viana <mj.viana1993@hotmail.com>
2024-05-23 23:10:57 +0100

    Update next.config.mjs

--- src/will-on-the-blockchain-frontend/next.config.mjs
+++ src/will-on-the-blockchain-frontend/next.config.mjs
@@ -2,32 +2,11 @@
 const nextConfig = {
   output: "export", // Outputs a Single-Page Application (SPA).
   distDir: "./dist", // Changes the build output directory to `./dist/`.
-  reactStrictMode: true, // Enables React Strict Mode.
-  // webpack: (config) => {
-  //   config.resolve.fallback = {fs: false, net: false, tls: false};
-  //   config.externals.push("pino-pretty", "encoding");
-  //   return config;
-  // },
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api/:path*",
-        headers: [
-          {key: "Access-Control-Allow-Credentials", value: "true"},
-          {key: "Access-Control-Allow-Origin", value: "*"},
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      },
-    ];
+  // reactStrictMode: true, // Enables React Strict Mode.
+  webpack: (config) => {
+    config.resolve.fallback = {fs: false, net: false, tls: false};
+    config.externals.push("pino-pretty", "encoding");
+    return config;
   },
 };
 
